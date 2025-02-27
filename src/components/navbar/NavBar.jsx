3d5ae75a0e9412a61179804ac0d0b5bb5ae1d0c8
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-02-27 16:31:38 -0500

    navbar animation updates

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,17 +5,15 @@ import MobileMenu from "./MobileMenu";
 const NavBar = () => {
   return (
     <header className="h-20">
-      <nav className="flex fixed top-0 inset-x-0 z-10 w-screen justify-center px-16 py-6 border-b-1 border-b-gray-400 bg-gray-800 text-white backdrop-blur">
-        {/*<nav className="fixed top-0 inset-x-0 z-10 flex w-full items-center px-16 py-6 justify-center content-center border-b border-gray-400 bg-gray-700 text-white backdrop-blur md:justify-items-stretch">*/}
-        {/* Small-Screen Menu */}
-
+      <nav className="fixed inset-x-0 top-0 z-10 flex w-screen border-b-1 border-b-gray-400 bg-gray-800 px-16 py-6 text-white backdrop-blur sm:justify-start md:justify-center">
         {/* Desktop Menu */}
-        <ul className="hidden md:flex gap-x-1">
+        <ul className="hidden gap-x-1 md:flex">
           {NavbarItems.map((item) => (
             <DesktopMenu menu={item} key={item.name} />
           ))}
         </ul>
 
+        {/* Small-Screen Menu */}
         <div className="md:hidden">
           <MobileMenu NavbarItems={NavbarItems} />
         </div>
