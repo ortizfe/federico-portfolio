2a3e7ca532879c1f0369c18c1c43460268504fd3
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-02-27 15:35:00 -0500

    wrapped app with browserrouter in test. fixed fails

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it } from "vitest";
 import App from "../src/App";
+import { BrowserRouter } from "react-router";
 
-describe("App", () => {
+describe("Spotify", () => {
   it("renders the App component", () => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
 
     screen.debug();
   });
